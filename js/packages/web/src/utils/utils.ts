462a1d445620deeba3b38e6c3dfdbfc83db3aa13
Kevin Rodriguez <_@kevinrodriguez.io>
2021-12-16 13:34:30 -0600

    feat: small stuff

--- js/packages/web/src/utils/utils.ts
+++ js/packages/web/src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { Connection, Transaction } from '@solana/web3.js';
+
 export const cleanName = (name?: string): string | undefined => {
   if (!name) {
     return undefined;
@@ -12,4 +14,21 @@ export const getLast = <T>(arr: T[]) => {
   }
 
   return arr[arr.length - 1];
+};
+
+export const calculateTransactionCost = async (
+  connection: Connection,
+  transaction: Transaction,
+) => {
+  const recentBlockhash = await connection.getRecentBlockhash();
+  const lamportsPerSignature =
+    recentBlockhash.feeCalculator.lamportsPerSignature; // Signature Cost
+  const signatureCount = 1; // Let's assume only 1 signature is needed
+  const initialCost = lamportsPerSignature * signatureCount;
+  const allCosts = await Promise.all(
+    transaction.instructions.map(instruction =>
+      connection.getMinimumBalanceForRentExemption(instruction.data.byteLength),
+    ),
+  );
+  return initialCost + allCosts.reduce((acc, curr) => acc + curr, 0);
 };
