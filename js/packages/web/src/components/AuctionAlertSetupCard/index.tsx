1b2af0730edea0bca355c220492e7f640fb3da95
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-04-21 16:39:47 +0100

    chore: prettier

--- js/packages/web/src/components/AuctionAlertSetupCard/index.tsx
+++ js/packages/web/src/components/AuctionAlertSetupCard/index.tsx
@@ -1,19 +1,16 @@
-import React, {FC, useEffect, useLayoutEffect, useRef, useState} from 'react';
+import React, { FC, useEffect, useLayoutEffect, useRef, useState } from 'react';
 
-import {
-  BlockchainEnvironment,
-  useNotifiClient,
-} from '@notifi-network/notifi-react-hooks'
+import { BlockchainEnvironment, useNotifiClient } from '@notifi-network/notifi-react-hooks';
 
 interface AuctionAlertSetupProps {
-  env: BlockchainEnvironment,
-  isWalletConnected: boolean,
-  dappId: string,
-  userWalletAddress: string | undefined,
-  storeName: string,
-  auctionWebUrl: string,
-  auctionAddress: string,
-  signerCallback: (message: Uint8Array) => Promise<Uint8Array>
+  env: BlockchainEnvironment;
+  isWalletConnected: boolean;
+  dappId: string;
+  userWalletAddress: string | undefined;
+  storeName: string;
+  auctionWebUrl: string;
+  auctionAddress: string;
+  signerCallback: (message: Uint8Array) => Promise<Uint8Array>;
 }
 
 const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupProps) => {
@@ -29,10 +26,11 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
   const [alertCardActive, setAlertCardActive] = useState(false);
   const [isEditing, setIsEditing] = useState(true);
   const [showSubscribeAlertMessage, setShowSubscribeAlertMessage] = useState(false);
-  const [notificationContainerClass, setNotificationContainerClass] = useState('notificationsContainer');
+  const [notificationContainerClass, setNotificationContainerClass] =
+    useState('notificationsContainer');
   const notificationsContainer = useRef<HTMLDivElement>(null);
-  const [requestedState, setRequestedState] = useState<InternalState>(InternalState.Uninitialized)
-  const [actualState, setActualState] = useState<InternalState>(InternalState.Uninitialized)
+  const [requestedState, setRequestedState] = useState<InternalState>(InternalState.Uninitialized);
+  const [actualState, setActualState] = useState<InternalState>(InternalState.Uninitialized);
   const [sourceId, setSourceId] = useState<string>('');
   const [filterId, setFilterId] = useState<string>('');
   const [emailAddress, setEmailAddress] = useState<string>('');
@@ -40,61 +38,68 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
   const [alertId, setAlertId] = useState<string>('');
   const [stateInAwait, setStateInAwait] = useState(false);
 
-  const { fetchData, logIn, loading, isAuthenticated, createAlert, deleteAlert, createMetaplexAuctionSource } =
-  useNotifiClient({
+  const {
+    fetchData,
+    logIn,
+    loading,
+    isAuthenticated,
+    createAlert,
+    deleteAlert,
+    createMetaplexAuctionSource,
+  } = useNotifiClient({
     dappAddress: props.dappId,
     walletPublicKey: props.userWalletAddress ? props.userWalletAddress : '',
     env: props.env,
   });
 
   useLayoutEffect(() => {
-      const updateSize = () => {
-          if (notificationsContainer.current) {
-              const containerWidth = notificationsContainer.current.offsetWidth;
-              const widthClass = containerWidth <= 400 ? 'ant-card-body notificationsContainerSm'
-                  : containerWidth <= 680 ? 'ant-card-body notificationsContainerMd'
-                      : containerWidth <= 1100 ? 'ant-card-body notificationsContainerLg'
-                          : 'ant-card-body';
-              setNotificationContainerClass(widthClass);
-          }
+    const updateSize = () => {
+      if (notificationsContainer.current) {
+        const containerWidth = notificationsContainer.current.offsetWidth;
+        const widthClass =
+          containerWidth <= 400
+            ? 'ant-card-body notificationsContainerSm'
+            : containerWidth <= 680
+            ? 'ant-card-body notificationsContainerMd'
+            : containerWidth <= 1100
+            ? 'ant-card-body notificationsContainerLg'
+            : 'ant-card-body';
+        setNotificationContainerClass(widthClass);
       }
+    };
 
-      window.addEventListener('resize', updateSize);
-      updateSize();
-      return () => window.removeEventListener('resize', updateSize);
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
   }, []);
 
-
   useEffect(() => {
-    const doWork = async() => {
+    const doWork = async () => {
       try {
-        console.log(isAuthenticated())
-        if (requestedState == actualState &&
-            requestedState == InternalState.Uninitialized &&
-            !loading) {
-              if (isAuthenticated()){
-                setRequestedState(InternalState.SyncedData)
-              }
-            }
-        await advanceToNextActualState()
+        console.log(isAuthenticated());
+        if (
+          requestedState == actualState &&
+          requestedState == InternalState.Uninitialized &&
+          !loading
+        ) {
+          if (isAuthenticated()) {
+            setRequestedState(InternalState.SyncedData);
+          }
+        }
+        await advanceToNextActualState();
       } catch (error) {
-        console.log("Exception caught: " + error);
+        console.log('Exception caught: ' + error);
         setRequestedState(InternalState.Uninitialized);
         setActualState(InternalState.Uninitialized);
       }
     };
 
     doWork();
-  }, [
-    requestedState,
-    setRequestedState,
-    actualState,
-    setActualState,
-    loading]);
+  }, [requestedState, setRequestedState, actualState, setActualState, loading]);
 
   const advanceToNextActualState = async function () {
-    console.log("a:" + actualState);
-    console.log("r: " + requestedState);
+    console.log('a:' + actualState);
+    console.log('r: ' + requestedState);
     if (actualState == requestedState || stateInAwait) {
       return;
     }
@@ -109,27 +114,27 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
         if (requestedState > actualState) {
           setActualState(InternalState.SigningInToNotifi);
         }
-       break;
+        break;
       case InternalState.SigningInToNotifi:
         if (!isAuthenticated()) {
           setStateInAwait(true);
-          await logIn({signMessage: props.signerCallback});
+          await logIn({ signMessage: props.signerCallback });
           setStateInAwait(false);
         }
         setActualState(InternalState.SignedInToNotifi);
-       break;
+        break;
       case InternalState.SignedInToNotifi: {
         setStateInAwait(true);
-        const {alerts, targetGroups} = await fetchData();
+        const { alerts, targetGroups } = await fetchData();
         setStateInAwait(false);
 
         if (requestedState > InternalState.SignedInToNotifi) {
-        setStateInAwait(true);
+          setStateInAwait(true);
           const s = await createMetaplexAuctionSource({
             auctionAddressBase58: props.auctionAddress,
-            auctionWebUrl: props.auctionWebUrl
+            auctionWebUrl: props.auctionWebUrl,
           });
-        setStateInAwait(false);
+          setStateInAwait(false);
 
           setSourceId(s.id!);
           const filter = s.applicableFilters.find((it) => {
@@ -140,18 +145,18 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
           if (!filter) {
             setRequestedState(InternalState.Uninitialized);
             setActualState(InternalState.Uninitialized);
-            throw "Failed to find appropriate Filter";
+            throw 'Failed to find appropriate Filter';
           }
 
           setFilterId(filter.id!);
-          
+
           if (alerts) {
             const alert = alerts.find((it) => {
               return it.name === `Auction: ${props.auctionAddress}`;
             });
 
             if (alert) {
-              console.log("Found alertId: " + alert.id!);
+              console.log('Found alertId: ' + alert.id!);
               setAlertId(alert.id!);
               setIsEditing(false);
               setAlertCardActive(true);
@@ -177,7 +182,7 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
 
           setActualState(InternalState.SyncedData);
         }
-       break;
+        break;
       }
       case InternalState.SyncedData:
         if (requestedState < actualState) {
@@ -186,14 +191,14 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
 
         if (requestedState == InternalState.UpdatedData) {
           if (alertId) {
-            setStateInAwait(true);    
-            await deleteAlert({alertId});
+            setStateInAwait(true);
+            await deleteAlert({ alertId });
             setStateInAwait(false);
             setAlertId('');
           }
 
           setStateInAwait(true);
-          console.log("Creating Notifi Alert");
+          console.log('Creating Notifi Alert');
           const res = await createAlert({
             filterId: filterId,
             sourceId: sourceId,
@@ -210,14 +215,14 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
             // TODO: Set error state
             setRequestedState(InternalState.Uninitialized);
             setActualState(InternalState.Uninitialized);
-            throw "Failed to create Alert";
+            throw 'Failed to create Alert';
           } else {
             setAlertId(res.id!);
           }
 
           setActualState(InternalState.UpdatedData);
         }
-       break;
+        break;
       case InternalState.UpdatedData:
         // TODO: Add eventing or other possible notifications here.
         // Reset back to idle state
@@ -227,33 +232,33 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
       default:
         break;
     }
-  }
+  };
 
   const toggleAlerts = (e: React.FormEvent<HTMLInputElement>) => {
-      if (requestedState != actualState) {
-        return;
+    if (requestedState != actualState) {
+      return;
+    }
+
+    setAlertCardActive(e.currentTarget.checked);
+
+    if (e.currentTarget.checked) {
+      setRequestedState(InternalState.SyncedData);
+    } else {
+      if (alertId) {
+        deleteAlert({ alertId });
+        setAlertId('');
       }
 
-      setAlertCardActive(e.currentTarget.checked);
-      
-      if (e.currentTarget.checked) {
-        setRequestedState(InternalState.SyncedData);
-      } else {
-        if (alertId) {
-          deleteAlert({alertId});
-          setAlertId('');
-        }
-
-        setRequestedState(InternalState.Uninitialized);
-      }
+      setRequestedState(InternalState.Uninitialized);
+    }
   };
 
   const subscribe = () => {
     setIsEditing(false);
-    setRequestedState(InternalState.UpdatedData)
+    setRequestedState(InternalState.UpdatedData);
     setShowSubscribeAlertMessage(true);
     setTimeout(() => {
-        setShowSubscribeAlertMessage(false)
+      setShowSubscribeAlertMessage(false);
     }, 5000);
   };
 
@@ -263,71 +268,106 @@ const AuctionAlertSetup: FC<AuctionAlertSetupProps> = (props: AuctionAlertSetupP
   };
 
   const onEmailAddressChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setEmailAddress(e.currentTarget.value)
-  }
+    setEmailAddress(e.currentTarget.value);
+  };
 
   const onPhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let val = e.target.value
+    let val = e.target.value;
     if (val.length > 1) {
-      val = val.substring(2)
+      val = val.substring(2);
     }
 
-    const re = /^[0-9\b]+$/
+    const re = /^[0-9\b]+$/;
     if (val === '' || (re.test(val) && val.length <= 10)) {
-      setPhoneNumber('+1' + val)
+      setPhoneNumber('+1' + val);
     }
-  }
+  };
 
   return (
-      <div className={notificationContainerClass} ref={notificationsContainer}>
-          <div className='getAlertsContainer'>
-              <div className='getAlertsToggleContainer'>
-                  <span className='ant-card-head-title'>Get Auction and Bid Alerts</span>
+    <div className={notificationContainerClass} ref={notificationsContainer}>
+      <div className="getAlertsContainer">
+        <div className="getAlertsToggleContainer">
+          <span className="ant-card-head-title">Get Auction and Bid Alerts</span>
 
-                  <div className='getAlertsToggle'>
-                      <label className='getAlertsSwitch'>
-                          <input disabled={!props.isWalletConnected} type="checkbox" checked={alertCardActive && props.isWalletConnected} onChange={toggleAlerts}/>
-                          <span className='getAlertsSwitchSlider'/>
-                      </label>
-                  </div>
-
-                  {(showSubscribeAlertMessage && alertId) && <div className='subscribedAlert'>
-                      <img alt="check" src="/img/check-icon.png" />
-                      <span>You’re subscribed for alerts </span>
-                  </div>}
-              </div>
-
-              <div className='getAlertsPoweredBy'>
-                  <span className='getAlertsPoweredByText'>Powered by</span>
-                  <img alt="Powered by Logo" src="/img/logo.png"/>
-              </div>
+          <div className="getAlertsToggle">
+            <label className="getAlertsSwitch">
+              <input
+                disabled={!props.isWalletConnected}
+                type="checkbox"
+                checked={alertCardActive && props.isWalletConnected}
+                onChange={toggleAlerts}
+              />
+              <span className="getAlertsSwitchSlider" />
+            </label>
           </div>
 
-          {alertCardActive && props.isWalletConnected && <div className='subscribeContainer'>
-              <div className='subscribeInput'>
-                  <div className='subscribeInputContainer'>
-                      <img alt="email" src="/img/email-logo.png"/>
-                      <input onChange={onEmailAddressChange} type="email" placeholder="Email address" value={emailAddress} className={!isEditing ? 'subscribedInput' : ''} readOnly={!isEditing}/>
-                  </div>
-              </div>
+          {showSubscribeAlertMessage && alertId && (
+            <div className="subscribedAlert">
+              <img alt="check" src="/img/check-icon.png" />
+              <span>You’re subscribed for alerts </span>
+            </div>
+          )}
+        </div>
 
-              <div className='subscribeInput'>
-                  <div className='subscribeInputContainer'>
-                      <img alt="phone" src="/img/mobile-logo.png" title="US phone numbers only. eg: +15551112222"/>
-                      <input onChange={onPhoneNumberChange} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone number" title="US phone numbers only. eg: +15551112222" value={phoneNumber} className={!isEditing ? 'subscribedInput' : ''} readOnly={!isEditing}/>
-                  </div>
-              </div>
-
-              {(!alertId || isEditing) && <div className='subscribeButton'>
-                  <button onClick={subscribe}>Subscribe</button>
-              </div>}
-
-              {alertId && !isEditing && <div className='editButton'>
-                  <button type="submit" onClick={editInfo}>Edit Info</button>
-              </div>}
-          </div>}
+        <div className="getAlertsPoweredBy">
+          <span className="getAlertsPoweredByText">Powered by</span>
+          <img alt="Powered by Logo" src="/img/logo.png" />
+        </div>
       </div>
-  )
+
+      {alertCardActive && props.isWalletConnected && (
+        <div className="subscribeContainer">
+          <div className="subscribeInput">
+            <div className="subscribeInputContainer">
+              <img alt="email" src="/img/email-logo.png" />
+              <input
+                onChange={onEmailAddressChange}
+                type="email"
+                placeholder="Email address"
+                value={emailAddress}
+                className={!isEditing ? 'subscribedInput' : ''}
+                readOnly={!isEditing}
+              />
+            </div>
+          </div>
+
+          <div className="subscribeInput">
+            <div className="subscribeInputContainer">
+              <img
+                alt="phone"
+                src="/img/mobile-logo.png"
+                title="US phone numbers only. eg: +15551112222"
+              />
+              <input
+                onChange={onPhoneNumberChange}
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                placeholder="Phone number"
+                title="US phone numbers only. eg: +15551112222"
+                value={phoneNumber}
+                className={!isEditing ? 'subscribedInput' : ''}
+                readOnly={!isEditing}
+              />
+            </div>
+          </div>
+
+          {(!alertId || isEditing) && (
+            <div className="subscribeButton">
+              <button onClick={subscribe}>Subscribe</button>
+            </div>
+          )}
+
+          {alertId && !isEditing && (
+            <div className="editButton">
+              <button type="submit" onClick={editInfo}>
+                Edit Info
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AuctionAlertSetup;
--- js/packages/web/src/components/AuctionCard/index.tsx
+++ js/packages/web/src/components/AuctionCard/index.tsx
@@ -64,7 +64,7 @@ import { DateTime } from 'luxon';
 import { ChevronRightIcon } from '@heroicons/react/solid';
 import { CrossMintButton } from '@crossmint/client-sdk-react-ui';
 import AuctionAlertSetup from '../AuctionAlertSetupCard';
-import {BlockchainEnvironment} from '@notifi-network/notifi-react-hooks'
+import { BlockchainEnvironment } from '@notifi-network/notifi-react-hooks';
 
 const { Text } = Typography;
 
@@ -941,7 +941,7 @@ export const AuctionCard = ({
   const showConnectToBidBtn = !hideDefaultAction && !wallet.connected;
 
   const connectionConfig = useConnectionConfig();
-  
+
   // Show the refund/reclaim/reedem bid button
   const showRedeemReclaimRefundBtn =
     showPlaceBidUI &&
@@ -966,9 +966,9 @@ export const AuctionCard = ({
     const signed = await wallet.signMessage(message);
     // Sollet Adapter signMessage returns Uint8Array
     if (signed instanceof Uint8Array) {
-      return signed
+      return signed;
     }
-  }
+  };
 
   return (
     <div>
@@ -1019,7 +1019,7 @@ export const AuctionCard = ({
             {/* todo: reduce opacity if starting bid */}
             <div className={cx('flex items-center text-xl')}>
               <svg
-                className="stroke-color-text mx-[5px] h-4 w-4 opacity-75"
+                className="mx-[5px] h-4 w-4 stroke-color-text opacity-75"
                 viewBox="0 0 16 16"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -1078,30 +1078,37 @@ export const AuctionCard = ({
         )}
       </Card>
 
-      <Card
-        bordered={false}
-        className="metaplex-margin-bottom-4 auction-card"
-        headStyle={{
-          borderBottom: !someoneWon ? '0' : '1px solid var(--color-border, #121212)',
-        }}
-        bodyStyle={{
-          padding: shouldHide ? 0 : 24,
-        }}>
-          {location && <AuctionAlertSetup env={connectionConfig.env == 'devnet' ? BlockchainEnvironment.DevNet : BlockchainEnvironment.MainNetBeta}
-            dappId={`${programIds().metaplex}.Holaplex`}
-            storeName={storefront.subdomain}
-            auctionAddress={auctionView.auction.pubkey}
-            auctionWebUrl={location}
-            isWalletConnected={wallet.connected}
-            signerCallback={async (m) => {
-              const res = await adapter(m)!;
-              if (res == undefined) {
-                return new Uint8Array();
+      {!auctionEnded && (
+        <Card
+          bordered={false}
+          className="metaplex-margin-bottom-4 auction-card"
+          title={'Notifications'}
+        >
+          {location && (
+            <AuctionAlertSetup
+              env={
+                connectionConfig.env == 'devnet'
+                  ? BlockchainEnvironment.DevNet
+                  : BlockchainEnvironment.MainNetBeta
               }
-              return res!;
-            }}
-            userWalletAddress={wallet ? wallet.publicKey?.toBase58() : undefined} />}
+              dappId={`${programIds().metaplex}.Holaplex`}
+              storeName={storefront.subdomain}
+              auctionAddress={auctionView.auction.pubkey}
+              auctionWebUrl={location}
+              isWalletConnected={wallet.connected}
+              signerCallback={async (m) => {
+                const res = await adapter(m)!;
+                if (res == undefined) {
+                  return new Uint8Array();
+                }
+                return res!;
+              }}
+              userWalletAddress={wallet ? wallet.publicKey?.toBase58() : undefined}
+            />
+          )}
         </Card>
+      )}
+
       <MetaplexModal visible={showWarningModal} onCancel={() => setShowWarningModal(false)}>
         <h3>
           Warning: There may be some items in this auction that still are required by the auction
@@ -1128,14 +1135,14 @@ const WinnerProfile = ({ bidderPubkey }: { handle?: string; bidderPubkey: string
           <div className="flex-shrink-0 pr-4">
             <Identicon size={48} address={bidderPubkey} />
           </div>
-          <div className="group-hover:text-primary text-color-text flex min-w-0 flex-1 justify-between">
+          <div className="flex min-w-0 flex-1 justify-between text-color-text group-hover:text-primary">
             <div className="text-color-text">
-              <p className=" group-hover:text-primary  text-color-text flex items-center  truncate font-medium">
+              <p className=" flex  items-center truncate font-medium  text-color-text group-hover:text-primary">
                 {bidderTwitterHandle ? '@' + bidderTwitterHandle : shortenAddress(bidderPubkey)}
               </p>
             </div>
           </div>
-          <div className="group-hover:text-primary text-color-text flex items-center">
+          <div className="flex items-center text-color-text group-hover:text-primary">
             <span className="block">View profile</span>
             <ChevronRightIcon className="h-5 w-5" viewBox="0 0 18 18" aria-hidden="true" />
           </div>
